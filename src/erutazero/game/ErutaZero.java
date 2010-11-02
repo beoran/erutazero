b26500a551cde7f8f86bcce5337acb5f94f0ac23
Beoran <beoran@rubyforge.org>
2010-11-02 22:23:33 +0100

    Netbeans project.

---
+++ src/erutazero/game/ErutaZero.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package erutazero.game;
+
+import javax.microedition.midlet.*;
+
+/**
+ * @author bjorn
+ */
+public class ErutaZero extends MIDlet {
+    public void startApp() {
+    }
+
+    public void pauseApp() {
+    }
+
+    public void destroyApp(boolean unconditional) {
+    }
+}
---
+++ src/erutazero/game/Screen.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package erutazero.game;
+
+/**
+ *
+ * @author bjorn
+ */
+public class Screen {
+
+}
