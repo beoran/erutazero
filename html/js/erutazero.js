9e3af147392192e76fc2625e416c2950fda773b9
Beoran <beoran@rubyforge.org>
2011-04-05 15:48:48 +0200

    Trying HTML5 approach.

--- html/js/erutazero.js
+++ html/js/erutazero.js
@@ -4,38 +4,45 @@ Erutazero = new Object();
 
 Erutazero.Game = new Object();
 Erutazero.Game.prototype  = Erutazero.Game
+
+Erutazero.Game.initialize = function() {
+  this._canvas  = Beja.id('canvas');
+  this._draw    = null;
+  if (this._canvas && this._canvas.getContext) {
+  //check whether browser support getting canvas context
+    this._draw = this._canvas.getContext('2d');
+  } else {
+    throw("Canvas not supported");
+  } 
+  var x = 10;
+  var y = 10;
+  
+  this._draw.fillStyle = "rgb(127,0,0)";
+  this._draw.fillRect(x, y, 100, 85);
+  
+  x += 120;
+  this._draw.strokeStyle = "rgb(0,0,0)";
+  this._draw.strokeRect(0, 0, 640, 480);
+  x = 10;
+  y += 120;
+  this._draw.fillStyle = "rgb(127,255,0)";
+  this._draw.font = "bold 26px serif";
+  this._draw.fillText('Test text ', x, y);
+
+  y += 20;
+  var img = new Image();
+  img.src = "../Content/Images/cos2a.gif";
+  img.onload = function () {
+      this._draw.drawImage(img, x, y);
+  }
+  
+}  
+
+
  
  
 Beja.onload = function () {
-  var _canvas = document.getElementById('canvas');
-  var _canvasContext = null;
-
-  if (_canvas && _canvas.getContext) {
-      //check whether browser support getting canvas context
-      _canvasContext = _canvas.getContext('2d');
-      var x = 10;
-      var y = 10;
-  
-      _canvasContext.fillStyle = "rgb(127,0,0)";
-      _canvasContext.fillRect(x, y, 100, 85);
-      
-      x += 120;
-      _canvasContext.strokeStyle = "rgb(0,0,0)";
-      _canvasContext.strokeRect(0, 0, 640, 480);
-      x = 10;
-      y += 120;
-      _canvasContext.fillStyle = "rgb(127,255,0)";
-      _canvasContext.font = "bold 26px sans-serif";
-      _canvasContext.fillText('Test text ', x, y);
-  
-      y += 20;
-      var img = new Image();
-      img.src = "../Content/Images/cos2a.gif";
-      img.onload = function () {
-          _canvasContext.drawImage(img, x, y);
-      }
-  }
-
+  Erutazero.Game.initialize();
 }
  
  
